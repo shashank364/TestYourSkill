efaf160d0c47ce4c60604153b6a0b58036a8249d
shashank <shashankshashu200@gail.com>
2020-11-18 15:58:50 +0530

    InitialCommit

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../components/Footer.css'
+
+function Footer(){
+    return(
+        <footer className="footer">
+            <h4>Shashank | 2020</h4>
+        </footer>
+    )
+}
+
+export default Footer;
---
+++ src/quizService/index.js
@@ -0,0 +1,375 @@
+const qBank = [
+  {
+    question:
+      " DBMS is a collection of ………….. that enables user to create and maintain a database.?   ",
+    answers: ["Keys", "Translators", "Program", "Language Activity"],
+    correct: "Program",
+    questionId: "099099"
+  },
+  {
+    question:
+      'In a relational schema, each tuple is divided into fields called?',
+    answers: ["Relations", "Domains", "Queries", "All of the above"],
+    correct: "Domains",
+    questionId: "183452"
+  },
+  {
+    question:
+      "In an ER model, ……………. is described in the database by storing its data?",
+    answers: ["Entity", "Attribute", "Relationship", " Notation"],
+    correct: "Entity",
+    questionId: "267908"
+  },
+  {
+    question: "DFD stands for?",
+    answers: [
+      "Data Flow Document",
+      "Data File Diagram",
+      "Data Flow Diagram",
+      "Non of the above"
+    ],
+    correct: "Data Flow Diagram",
+    questionId: "333247"
+  },
+  {
+    question: "A top-to-bottom relationship among the items in a database is established by a?",
+    answers: ["Hierarchical schema", "Network schema", "Relational Schema", "All of the above"],
+    correct: "Hierarchical schema",
+    questionId: "496293"
+  },
+  {
+    question:
+      "………………… is a full form of SQL?",
+    answers: [
+      "Standard query language",
+      "Sequential query language",
+      "Structured query language",
+      "Server side query language"
+    ],
+    correct: "Structured query language",
+    questionId: "588909"
+  },
+  {
+    question:
+      ' ………. keyword is used to find the number of values in a columnt?',
+    answers: ["SUM", "COUNT", "AVG", "MAX"],
+    correct: "COUNT",
+    questionId: "648452"
+  },
+  {
+    question: "A ……… is used to define overall design of the database?",
+    answers: ["Schema", "application program", "data definition language", "code"],
+    correct: "Schema",
+    questionId: "786649"
+  },
+  {
+    question:
+      "Key to represent relationship between tables is called?",
+    answers: ["primary key", "secondary key", "foreign key", "none of the above"],
+    correct: "foreign key",
+    questionId: "839754"
+  },
+  {
+    question:
+      "Grant and revoke are ……. statements.?",
+    answers: [
+      "DDL",
+      "TCL",
+      "DCL",
+      "DML"
+    ],
+    correct: "DCL",
+    questionId: "98390"
+  },
+  {
+    question: "Which of the following is not an operating system?",
+    answers: ["Windows", "Linux", "Oracle", "DOS"],
+    correct: "Oracle",
+    questionId: "1071006"
+  },
+  {
+    question: "Process is?",
+    answers: ["contents of main memory", "a program in execution", "	a job in secondary memory", "None of the above"],
+    correct: "a program in execution",
+    questionId: "1174154"
+  },
+  {
+    question: "In which of the storage placement strategies a program is placed in the largest available hole in the main memory?",
+    answers: ["best fit", "first fit", "worst fit", "buddy"],
+    correct: "worst fit",
+    questionId: "1226535"
+  },
+  {
+    question: "Which of the following software types is used to simplify using systems software?",
+    answers: ["operating environment", " timesharing", "multitasking", "spreadsheet"],
+    correct: "timesharing",
+    questionId: "1310938"
+  },
+  {
+    question: "What scheduling algorithm allows processes that are logical runnable to be temporarily suspended?",
+    answers: ["preemptive scheduling", "non-preemptive scheduling", "FIFO", "FCFS"],
+    correct: "preemptive scheduling",
+    questionId: "1436873"
+  },
+  {
+    question: "The computational technique used to compute the disk storage address of individual records is called:?",
+    answers: ["bubble memory", "key fielding", "dynamic reallocation", "hashing"],
+    correct: "hashing",
+    questionId: "1515110"
+  },
+  {
+    question: "Indicate which is a pre-emptive scheduling algorithm?",
+    answers: ["Round-robin", "Shortest-job-next", "Priority-based", "	All of the above"],
+    correct: "Round-robin",
+    questionId: "1642728"
+  },
+  {
+    question:
+      "Which of the following, is necessary to work on a computer? ",
+    answers: ["Compiler", "Operating system", "Assembly", "Interpreter"],
+    correct: "Operating system",
+    questionId: "1747256"
+  },
+  {
+    question:
+      "Access time is the highest in the case of?",
+    answers: ["floppy disk", "cache", "swapping devices", "magnetic disk"],
+    correct: "magnetic disk",
+    questionId: "1822532"
+  },
+  {
+    question: "Banker's algorithm for resource allocation deals with?",
+    answers: [
+      "deadlock prevention",
+      "deadlock avoidance",
+      "deadlock recovery",
+      "mutual exclusion"
+    ],
+    correct: "deadlock avoidance",
+    questionId: "195075"
+  },
+  {
+    question: 'For a binary search algorithm to work, it is necessary that the array (list) must be?',
+    answers: ["sorted", "unsorted", "in a heap", "popped out of stack"],
+    correct: "sorted",
+    questionId: "2019778"
+  },
+  {
+    question: "What data structure is used for depth first traversal of a graph?",
+    answers: ["queue", "stack", "list", "dictonary"],
+    correct: "stack",
+    questionId: "2134343"
+  },
+  {
+    question: "Which of the following algorithm is not stable?",
+    answers: [
+      "Bubble Sort",
+      "Quick Sort",
+      "Merge Sort",
+      "Insertion Sort"
+    ],
+    correct: "Quick Sort",
+    questionId: "2210799"
+  },
+  {
+    question:
+      "Time required to merge two sorted lists of size m and n, is?",
+    answers: ["Ο(m | n)", "Ο(m + n)", "Ο(m log n)", "Ο(n log m)"],
+    correct: "Ο(m + n)",
+    questionId: "2379831"
+  },
+  {
+    question: "Tower of hanoi is a classic example of?",
+    answers: ["divide and conquer", "recursive approach", "B but not A", "Both A & B"],
+    correct: "Both A & B",
+    questionId: "2426418"
+  },
+  {
+    question: "Which of the following sorting algorithms can be used to sort a random linked list with minimum time complexity?",
+    answers: ["Insertion Sort", "Quick Sort", "Heap Sort", "Merge Sort"],
+    correct: "Merge Sort",
+    questionId: "2510086"
+  },
+  {
+    question: "The minimum number of comparisons required to determine if an integer appears more than n/2 times in a sorted array of n integers is?",
+    answers: [
+      "Θ(n)",
+      "Θ(logn)",
+      "Θ(log*n)",
+      "Θ(1)"
+    ],
+    correct: "Θ(logn)",
+    questionId: "2685745"
+  },
+  {
+    question:
+      "Which one of the following is an application of Stack Data Structure?",
+    answers: ["Managing function calls", "The stock span problem", "Arithmetic expression evaluation", "All of the above"],
+    correct: "All of the above",
+    questionId: "2796884"
+  },
+  {
+    question: "Stack is also called as?",
+    answers: [" Last in first out", "First in last out", "Last in last out", "First in first out"],
+    correct: "First in last out",
+    questionId: "2838900"
+  },
+  {
+    question: 'Which type of topology is best suited for large businesses which must carefully control and coordinate the operation of distributed branch outlets?',
+    answers: ["Ring", "Local area", " Hierarchical", " Star"],
+    correct: "Star",
+    questionId: "298060"
+  },
+  {
+    question: "Which of the following transmission directions listed is not a legitimate channel?",
+    answers: ["Simplex", "Half Duplex", "Full Duplex", "Double Duplex"],
+    correct: "Double Duplex",
+    questionId: "3096579"
+  },
+  {
+    question: "The location of a resource on the internet is given by its?",
+    answers: [
+      "Protocol",
+      "URL",
+      "E-mail address",
+      "ICQ"
+    ],
+    correct: "URL",
+    questionId: "3182461"
+  },
+  {
+    question: "The term HTTP stands for?",
+    answers: [
+      "Hyper terminal tracing program",
+      "Hypertext tracing protocol",
+      "Hypertext transfer protocol",
+      "Hypertext transfer program"
+    ],
+    correct: "Hypertext transfer protocol",
+    questionId: "3239112"
+  },
+  {
+    question: "A proxy server is used as the computer?",
+    answers: ["with external access", "acting as a backup", "performing file handling", "accessing user permissions"],
+    correct: "with external access",
+    questionId: "3318503"
+  },
+  {
+    question:
+      'Which software prevents the external access to a system',
+    answers: ["Firewall", "Gateway", "Router", "Virus checker"],
+    correct: "Firewall",
+    questionId: "3410327"
+  },
+  {
+    question: "Which one of the following is the most common internet protocol?",
+    answers: ["HTML", "NetBEUI", "TCP/IP", "IPX/SPX"],
+    correct: "TCP/IP",
+    questionId: "3542596"
+  },
+  {
+    question: "The term FTP stands for",
+    answers: ["File transfer program", "File transmission protocol", "File transfer protocol", "File transfer protection"],
+    correct: "File transfer protocol",
+    questionId: "3667517"
+  },
+  {
+    question: "Which one of the following is not a network topology?",
+    answers: [
+      "Star",
+      "Ring",
+      "Bus",
+      "Peer to Peer"
+    ],
+    correct: "Peer to Peer",
+    questionId: "3791672"
+  },
+  {
+    question: "The maximum length (in bytes) of an IPv4 datagram is?",
+    answers: ["32", "1024", "65535", "512"],
+    correct: "65535",
+    questionId: "3893585"
+  },
+  {
+    question: 'When the mail server sends mail to other mail servers it becomes ___?',
+    answers: ["SMTP client", "SMTP server", "Peer", "Master"],
+    correct: "SMTP client",
+    questionId: "3913430"
+  },
+  {
+    question: "Which of the following type of class allows only one object of it to be created?",
+    answers: ["Virtual class", "Abstract class", "Singleton class", "Friend class"],
+    correct: "Singleton class",
+    questionId: "4049121"
+  },
+  {
+    question:
+      "Which of the following is not the member of class?",
+    answers: ["Static function", "Friend function", "Const function", "Virtual function"],
+    correct: "Friend function",
+    questionId: "4150746"
+  },
+  {
+    question:
+      "The compiler checks the type of reference in the object and not the type of object?",
+    answers: ["Inheritance ", "Polymorphism", "Abstraction", "Encapsulation"],
+    correct: "Polymorphism",
+    questionId: "4235063"
+  },
+  {
+    question: "What is the name of the method used to start a thread execution?",
+    answers: ["init()", "start();", "	run();", "resume();"],
+    correct: "start();",
+    questionId: "4321002"
+  },
+  {
+    question: "Which of the following is not a Java features?",
+    answers: [
+      "Dynamic",
+      "Architecture Neutral",
+      "Use of pointers",
+      "Object-oriented"
+    ],
+    correct: "Use of pointers",
+    questionId: "4442286"
+  },
+  {
+    question: "The \u0021 article referred to as a?",
+    answers: [
+      "Unicode escape sequence",
+      "Octal escape",
+      "Hexadecimal",
+      "Line feed"
+    ],
+    correct: "Unicode escape sequence",
+    questionId: "4564430"
+  },
+  {
+    question: "What is the return type of the hashCode() method in the Object class?",
+    answers: ["Object", "int", "void", "long"],
+    correct: "int",
+    questionId: "4630606"
+  },
+  {
+    question:
+      'Evaluate the following Java expression, if x=3, y=5, and z=10: "++z + y - y + z + x++?"',
+    answers: ["24", "23", "20", "25"],
+    correct: "24",
+    questionId: "4795960"
+  },
+  {
+    question: " Which of the following tool is used to generate API documentation in HTML format from doc comments in source code?",
+    answers: ["javap tool", "javaw command", "Javadoc tool", "javah command"],
+    correct: "Javadoc tool",
+    questionId: "4811162"
+  },
+  {
+    question: "Which method of the Class.class is used to determine the name of a class represented by the class object as a String?",
+    answers: ["getClass()", "intern()", "getName()", "toString()"],
+    correct: "getName()",
+    questionId: "4982981"
+  }
+];
+
+export default (n = 5) =>
+  Promise.resolve(qBank.sort(() => 0.5 - Math.random()).slice(0, n));
